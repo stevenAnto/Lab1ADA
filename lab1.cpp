21603aef0e35f6f0042d4f8c4c4f1f87036944f7
stv <79427465+stevenAnto@users.noreply.github.com>
2022-09-27 18:47:11 -0500

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -19,3 +19,22 @@ int main()
   cout << multiplicado;
   return multiplicado;
 }
+
+// Ejercicio2
+#include <iostream>
+
+using namespace std;
+
+
+
+void mostrarOrden( int array[2]){
+    //int tamanio = sizeof(array)/ sizeof(int);
+    for (int i = 0; i < 5 ;i++){
+        cout << i <<"->"<< array[i]<<"\n";
+    }
+}
+int main()
+{
+    int arrayPrueba []={1,2,8,9,5};
+    mostrarOrden (arrayPrueba);
+}
